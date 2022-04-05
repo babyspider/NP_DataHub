bfbb09758f9281c33e7777bef6fc97994bc7ddeb
lissaa <arianalissak@gmail.com>
2022-04-05 13:08:15 -0400

    ran a basic kmeans grouping on revenues, assets liabilities for each nonprofit in the data set

--- Data/dataAnalysis.py
+++ Data/dataAnalysis.py
@@ -1,10 +1,11 @@
 # import statements
-
 import csv
 import statistics
-
+from sklearn.cluster import KMeans
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 
 # importing the file to analyze and putting it in a list
 file = "NTEE_OS_Housing_Master_OSTeam - Master.csv"
@@ -21,32 +22,42 @@ for x in range(0, len(values)):
     if values[x][364] != "":
         liabilities.append(float(values[x][364]))
 
-# pulling and converting liability values
+# pulling and converting asset values
 assets = []
 for x in range(0, len(values)):
     if values[x][363] != "":
         assets.append(float(values[x][363]))
+
 # pulling and converting revenue values
 revenues = []
 for x in range(0, len(values)):
-    if values[x][343] != "":
-        revenues.append(float(values[x][343]))
+    if values[x][345] != "":
+        revenues.append(float(values[x][345]))
 
-# finding median of assets and liabilities
+#three values grouped by nonprofit
+test = []
+for x in range(0, len(values)):
+    if values[x][364] != "" and values[x][345] != ""  and values[x][363] != "":
+        test.append([float(values[x][364]), float(values[x][363]), float(values[x][345])])
+# finding median of assets and liabilities using stats library
 medLiabilities = statistics.median(liabilities)
 
-
+#
+graph = plt.figure()
+km = KMeans(n_clusters = 3, n_init = 10, init = "random", verbose = 1)
+km.fit(test)
 # running knn algorithm on assets vs. liabilities
+'''
 landaNeigh = KNeighborsClassifier(n_neighbors=5)
 landaSet = [[]]
 landaSet.append(liabilities)
 landaSet.append(assets)
-landaNeigh.fit(assets, liabilities, multi_output=False)
-landaNeigh.reshape(-1, 1)
+landaNeigh.fit(assets, liabilities)
 # running knn on assets and revenues
-landrNeigh = KNeighborsClassifier(n_neighbors=5, multi_output=False)
+landrNeigh = KNeighborsClassifier(n_neighbors=5)
 landrNeigh.fit(assets, revenues)
 landrNeigh.reshape(-1, 1)
 # comparing one np to the median in liabilities
 # fig = px.scatter(x=landaNeigh[0], y=landaNeigh[1])
 # fig.show()
+''' 
