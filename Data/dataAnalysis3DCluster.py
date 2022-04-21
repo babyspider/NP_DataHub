46e4ddec369833221f149fbd3f65bd0f0e922e6d
danushgupta <danushgupta@gmail.com>
2022-04-21 16:09:20 -0400

    ran linter

--- Data/dataAnalysis3DCluster.py
+++ Data/dataAnalysis3DCluster.py
@@ -1,34 +1,38 @@
 # import statements
 import csv
 import statistics
-from sklearn.cluster import KMeans
+
+import matplotlib
+import matplotlib.pyplot as plt
+import mpld3
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt, mpld3
-import matplotlib
 
-#so you dont need a gui 
-matplotlib.use('Agg') 
+# so you dont need a gui
+matplotlib.use("Agg")
 
 # function that formats coordinates for plotting
 def convertToGraph(listy):
-	x = []
-	y = []
-	z = []
-	for i in range(0, len(listy)):
-		x.append(listy[i][0])
-		y.append(listy[i][1])
-		z.append(listy[i][2])
-	return [x, y, z]
+    x = []
+    y = []
+    z = []
+    for i in range(0, len(listy)):
+        x.append(listy[i][0])
+        y.append(listy[i][1])
+        z.append(listy[i][2])
+    return [x, y, z]
+
 
 # function to create lists from values
 def pullToList(values, i):
-	listy = []
-	for x in range(0, len(values)):
-		if values[x][i] != "":
-			listy.append(float(values[x][i]))
-	return listy
+    listy = []
+    for x in range(0, len(values)):
+        if values[x][i] != "":
+            listy.append(float(values[x][i]))
+    return listy
+
 
 # importing the file to analyze and putting it in a list
 file = "NTEE_OS_Housing_Master_OSTeam - Master.csv"
@@ -49,7 +53,7 @@ assets = pullToList(values, 363)
 # pulling and converting revenue values
 revenues = pullToList(values, 345)
 
-#values grouped by nonprofit
+# values grouped by nonprofit
 test = []
 for x in range(0, len(assets)):
     if assets[x] != "" and liabilities[x] != "":
@@ -59,25 +63,25 @@ for x in range(0, len(assets)):
 medLiabilities = statistics.median(liabilities)
 
 
-#defining and running the kmeans
+# defining and running the kmeans
 clusters = 4
 graph = plt.figure()
-ax = graph.add_subplot(projection='3d')
-km = KMeans(n_clusters = clusters, n_init = 10, init = "random")
+ax = graph.add_subplot(projection="3d")
+km = KMeans(n_clusters=clusters, n_init=10, init="random")
 label = km.fit_predict(test)
- 
 
-#graphing clusters
-colors = ['pink', 'purple', 'red', 'blue', 'green', 'black', 'orange', 'yellow']
+
+# graphing clusters
+colors = ["pink", "purple", "red", "blue", "green", "black", "orange", "yellow"]
 for c in range(0, clusters):
-	clustery = []
-	for l in range(0, len(label)):
-		if label[l] == c:
-			clustery.append(test[l])
-	clusterPlot = convertToGraph(clustery)
-	print(clusterPlot)
-	ax.scatter(clusterPlot[0], clusterPlot[1], clusterPlot[2], color = colors[c])
-	print("\n\n New CLuster \n\n")
+    clustery = []
+    for l in range(0, len(label)):
+        if label[l] == c:
+            clustery.append(test[l])
+    clusterPlot = convertToGraph(clustery)
+    print(clusterPlot)
+    ax.scatter(clusterPlot[0], clusterPlot[1], clusterPlot[2], color=colors[c])
+    print("\n\n New CLuster \n\n")
 
 # save graph as image
 plt.savefig("plot.png")
--- Data/dataAnalysisGraphOnServer.py
+++ Data/dataAnalysisGraphOnServer.py
@@ -1,32 +1,36 @@
 # import statements
 import csv
 import statistics
-from sklearn.cluster import KMeans
+
+import matplotlib
+import matplotlib.pyplot as plt
+import mpld3
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt, mpld3
-import matplotlib
 
-#so you dont need a gui 
-matplotlib.use('Agg') 
+# so you dont need a gui
+matplotlib.use("Agg")
 
 # function that formats coordinates for plotting
 def convertToGraph(listy):
-	x = []
-	y = []
-	for i in range(0, len(listy)):
-		x.append(listy[i][0])
-		y.append(listy[i][1])
-	return [x, y]
+    x = []
+    y = []
+    for i in range(0, len(listy)):
+        x.append(listy[i][0])
+        y.append(listy[i][1])
+    return [x, y]
+
 
 # function to create lists from values
 def pullToList(values, i):
-	listy = []
-	for x in range(0, len(values)):
-		if values[x][i] != "":
-			listy.append(float(values[x][i]))
-	return listy
+    listy = []
+    for x in range(0, len(values)):
+        if values[x][i] != "":
+            listy.append(float(values[x][i]))
+    return listy
+
 
 # importing the file to analyze and putting it in a list
 file = "NTEE_OS_Housing_Master_OSTeam - Master.csv"
@@ -47,7 +51,7 @@ assets = pullToList(values, 363)
 # pulling and converting revenue values
 revenues = pullToList(values, 345)
 
-#values grouped by nonprofit
+# values grouped by nonprofit
 test = []
 for x in range(0, len(assets)):
     if assets[x] != "" and liabilities[x] != "":
@@ -57,22 +61,22 @@ for x in range(0, len(assets)):
 medLiabilities = statistics.median(liabilities)
 
 
-#defining and running the kmeans
+# defining and running the kmeans
 clusters = 8
 graph = plt.figure()
-km = KMeans(n_clusters = clusters, n_init = 10, init = "random")
+km = KMeans(n_clusters=clusters, n_init=10, init="random")
 label = km.fit_predict(test)
- 
 
-#graphing clusters
-colors = ['pink', 'purple', 'red', 'blue', 'green', 'black', 'orange', 'yellow']
+
+# graphing clusters
+colors = ["pink", "purple", "red", "blue", "green", "black", "orange", "yellow"]
 for c in range(0, clusters):
-	clustery = []
-	for l in range(0, len(label)):
-		if label[l] == c:
-			clustery.append(test[l])
-	clusterPlot = convertToGraph(clustery)
-	plt.scatter(clusterPlot[0], clusterPlot[1], color = colors[c])
+    clustery = []
+    for l in range(0, len(label)):
+        if label[l] == c:
+            clustery.append(test[l])
+    clusterPlot = convertToGraph(clustery)
+    plt.scatter(clusterPlot[0], clusterPlot[1], color=colors[c])
 
 # save graph as image
 mpld3.show()
